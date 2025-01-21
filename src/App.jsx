dcc7131105c3369858446bb83875a33ee6c5f2ac
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-01-20 23:11:03 -0300

    Create responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header"
+import { useState } from "react"
+
 export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <>
+      {/* bg from https://bg.ibelick.com/ */}
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <Header />
+    </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <header className="sticky top-0 z-50 flex justify-between items-center py-6 px-8 md:px-32 bg-black/55 drop-shadow-md backdrop-blur-md font-roboto">
+            {/* Replace with real logo after rebrand */}
+            <a className="text-white font-bold hover:scale-105 transition-all">
+                MLRH
+            </a>
+
+            <ul className="hidden xl:flex items-center gap-12 text-base text-gray-200 font-roboto">
+                <li className="p-3 hover:font-bold">Home</li>
+                <li className="p-3 hover:font-bold">Currículo</li>
+                <li className="p-3 hover:font-bold">Vagas</li>
+                <li className="p-3 hover:font-bold">Serviços</li>
+                <li className="p-3 hover:font-bold">Sobre nós</li>
+                <li className="p-3 hover:font-bold">Contato</li>
+            </ul>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-6 xl:hidden block cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+
+            <div className={`absolute text-white xl:hidden top-[72px] left-0 w-full bg-black/55 backdrop-blur-md flex flex-col items-center font-roboto text-lg transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`} style={{ transtion: "transform 0.3s ease, opacity 0.3s ease" }}>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Home</li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Currículo</li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Vagas</li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Serviços</li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Sobre</li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Contato</li>
+            </div>
+        </header>
+    )
+}
