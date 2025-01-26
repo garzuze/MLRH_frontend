3c72f341afd35aa146750ec397aef2a3cbdc3a51
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-01-26 09:12:01 -0300

    Create vacancy component

---
+++ src/components/Vacancy.jsx
@@ -0,0 +1,19 @@
+export default function Vacancy({ profile }) {
+    return (
+        <div class="max-w-sm p-6 bg-neutral-950 border border-neutral-800 rounded-lg shadow-sm">
+            <a href="">
+                <h5 class="mb-2 text-2xl font-medium tracking-tight text-zinc-50">{profile.position}</h5>
+            </a>
+            <p class="mb-3 font-normal text-zinc-300 line-clamp-3">
+                {profile.activities}
+            </p>
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-zinc-100 rounded-lg hover:bg-zinc-300 focus:ring-4 focus:outline-none focus:ring-indigo-950">
+                Aplicar
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+            </a>
+        </div>
+
+    )
+}
