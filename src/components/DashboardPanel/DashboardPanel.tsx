9655c321405aa5d51218853c2017f8b080bdb0d0
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-18 09:22:55 -0300

    refactor: make TopBar component reusable

--- src/components/DashboardPanel/DashboardPanel.tsx
+++ src/components/DashboardPanel/DashboardPanel.tsx
@@ -1,10 +1,15 @@
 import Grid from "./Grid";
 import TopBar from "./TopBar";
+import { useAuth } from "../../context/AuthContext";
+import getDate from "./getDate";
 
 export default function DashboardPanel() {
+    const { user } = useAuth();
+    const currentDate = getDate();
     return (
         <div className="bg-white border rounded-lg pb-4 shadow h-[220vh] ">
-            <TopBar />
+
+            <TopBar title={`Bom dia, ${user?.first_name}!`} subtitle={currentDate} />
             <Grid />
         </div>
     )
--- src/components/DashboardPanel/TopBar.tsx
+++ src/components/DashboardPanel/TopBar.tsx
@@ -1,16 +1,14 @@
 import { useAuth } from "../../context/AuthContext"
 import getDate from "./getDate";
 
-export default function TopBar() {
-    const { user } = useAuth();
-    const currentDate = getDate()
+export default function TopBar({ title, subtitle }: { title: string, subtitle: string}) {
 
     return (
         <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
             <div className="flex items-center justify-between p-0.5">
                 <div>
-                    <span className="text-sm font-bold block">Bom dia, {user?.first_name}!</span>
-                    <span className="text-xs block text-stone-500">{currentDate}</span>
+                    <span className="text-sm font-bold block">{title}</span>
+                    <span className="text-xs block text-stone-500">{subtitle}</span>
                 </div>
             </div>
         </div>
