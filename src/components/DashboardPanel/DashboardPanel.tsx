b91dcb15d17773c4c8ff0b1ea5e65a92502bfa39
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-18 07:19:29 -0300

    feat: finish DashBoard panel
    Inspired by https://youtu.be/vdxnBKRD7kU?si=vcasnRF_3_tQ1L8n

--- src/components/DashboardPanel/DashboardPanel.tsx
+++ src/components/DashboardPanel/DashboardPanel.tsx
@@ -1,7 +1,11 @@
+import Grid from "./Grid";
+import TopBar from "./TopBar";
+
 export default function DashboardPanel() {
     return (
         <div className="bg-white border rounded-lg pb-4 shadow h-[220vh] ">
-            
+            <TopBar />
+            <Grid />
         </div>
     )
 }
