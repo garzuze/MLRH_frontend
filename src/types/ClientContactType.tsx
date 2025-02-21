4d9532d0cd2559642ac42a848ed14cf67adcc910
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-21 10:23:02 -0300

    refactor: create type ClientContactType

---
+++ src/types/ClientContactType.tsx
@@ -0,0 +1,9 @@
+export interface ClientContactType {
+    id: number;
+    client: number;
+    name: string;
+    department: string;
+    phone: string;
+    email: string;
+    status: boolean
+}
