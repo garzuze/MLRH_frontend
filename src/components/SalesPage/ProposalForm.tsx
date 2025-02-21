34faa9aef15d98f81a61a0526ff76c2ebd5ae536
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-02-21 10:24:05 -0300

    refactor: fetch client contact data from database

--- src/components/SalesPage/ProposalForm.tsx
+++ src/components/SalesPage/ProposalForm.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "../../contexts/AuthContext";
 import { useEffect, useState } from "react";
 import Snackbar from "../ui/Snackbar";
 import { ClientType } from "../../types/ClientType";
+import { ClientContactType } from "../../types/ClientContactType";
 
 export default function ProposalForm() {
     const { client } = useClient();
@@ -13,6 +14,7 @@ export default function ProposalForm() {
     const [snackbarMessage, setSnackbarMessage] = useState("");
 
     const [clientData, setClientData] = useState<ClientType | null>(null);
+    const [clientContactData, setClientContactData] = useState<ClientContactType[] | null>(null);
 
 
     const getClientData = async (clientId: number) => {
@@ -46,8 +48,40 @@ export default function ProposalForm() {
         }
     }
 
+    const getClientContactData = async (clientId: number) => {
+        try {
+            const axiosClient = axios.create({
+                baseURL: "http://127.0.0.1:8000/",
+            });
+
+            const config: AxiosRequestConfig = {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                } as RawAxiosRequestHeaders
+            };
+
+            const response: AxiosResponse = await axiosClient.get(`/clients/get_client_contacts/?q=${clientId}`, config);
+            console.log(response);
+            if (response.status === 200) {
+                setClientContactData(response.data)
+                if (response.data.length > 1) {
+
+                }
+            } else {
+                setSnackbarMessage("Opsss, alguma coisa deu errado...")
+                setIsSnackbarOpen(false)
+            }
+
+        } catch (error) {
+            console.log(error)
+            setSnackbarMessage("Opsss, alguma coisa deu errado...")
+            setIsSnackbarOpen(false)
+        }
+    }
+
     useEffect(() => {
         getClientData(client!.id);
+        getClientContactData(client!.id);
     }, [])
 
     return (
@@ -55,6 +89,10 @@ export default function ProposalForm() {
             {clientData?.corporate_name}
             <br></br>
             {clientData?.address}
+            <br></br>
+            {clientContactData ? (clientContactData.length > 1 ? (
+                clientContactData[0].name
+            ) : null) : null}
             <Snackbar
                 message={snackbarMessage}
                 isOpen={isSnackbarOpen}
