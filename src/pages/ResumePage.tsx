b3268cb9260176b75d060ffce76d519a11d05716
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-03-06 14:50:56 -0300

    feat: create simple Resume form

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,9 +1,15 @@
-import { useEffect, useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
-import { ResumeType } from "../types/ResumeType";
-import { mlrhUser } from "../types/TokenResponse";
-import { axiosClient, axiosConfig } from "../utils/constants";
+import { useEffect } from "react";
 import { ResumeProvider, useResume } from "../contexts/ResumeContext";
+import Title from "../components/ui/Title";
+import React from "react";
+import {
+    educationLevels,
+    languageLevels,
+    status,
+} from "../utils/constants";
+
+import { ResumeType } from "../types/ResumeType";
+import { ResumeForm } from "../components/ResumePage/ResumeForm";
 
 export default function ResumePage() {
     return (
@@ -13,16 +19,36 @@ export default function ResumePage() {
     )
 }
 
+
+export interface ResumeFormProps {
+    resume: ResumeType;
+}
+
 function Resume() {
     const { resume } = useResume();
 
     useEffect(() => {
         console.log("Resume state updated:", resume?.name);
     }, [resume]);
-    console.log(resume?.name);
+
+    if (resume) {
+        return (
+            <main className="w-full mx-auto">
+                <div className="px-6 py-8 font-roboto bg-gradient-to-br from-neutral-950 via-neutral-900 to-indigo-900">
+                    <Title variant="h3" text={`Seja bem vindo, ${resume.name}!`} className="text-center pb-2" />
+                    <p className="mb-3 font-normal text-zinc-300 line-clamp-3 text-center">Estas são as informações do seu currículo:</p>
+                    <div className="w-full bg-neutral-950 border border-neutral-800 rounded-lg shadow md:mt-0 md text-zinc-50 p-4 xl:max-w-screen-xl mx-auto">
+                        <ResumeForm resume={resume} />
+                    </div>
+                </div>
+
+            </main>
+        );
+    }
     return (
-        <div className="text-white">
-            {resume?.name || "carregando...."}
-        </div>
-    );
+        <main className="w-full mx-auto text-white">
+            <Title variant={"h3"} text={"Você precisa estar logado para acessar seu currículo!"} ></Title>
+        </main>
+    )
+
 }
