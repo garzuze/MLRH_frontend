0c0921210f1940744e0b1c63e21f9611dea9e8f3
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-03-10 08:28:32 -0300

    refactor: create ReportType

---
+++ src/types/ReportType.tsx
@@ -0,0 +1,14 @@
+export interface ReportType {
+    id: number;
+    profile: number;
+    resume: number;
+    testResult: string;
+    personalFamilyContext: string;
+    educationalBackground: string;
+    professionalSummary: string;
+    candidateProfile: string;
+    careerObjectives: string;
+    finalConsiderations: string;
+    agreedSalary: number;
+    candidateStartDate: string;
+}
