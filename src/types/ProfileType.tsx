b027471ea8395117c3c8229392e3e6b6d937d39c
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-03-10 08:28:25 -0300

    refactor: create ProfileType

--- src/types/ProfileType.tsx
+++ src/types/ProfileType.tsx
@@ -1,5 +1,33 @@
-export interface Profile {
-    id: number,
-    position: string,
-    activities: string,
+import { EducationLevelAbbreviation, GenderAbbreviation, maritalStatusAbbreviation, statusAbbreviation } from "../utils/constants";
+
+export interface ProfileType {
+    id: number;
+    client: number;
+    clientContact: number;
+    position: number;
+    fee: number;
+    strRepresentation: string;
+    maritalStatus: maritalStatusAbbreviation;
+    educationLevel: EducationLevelAbbreviation;
+    date: string;
+    status: statusAbbreviation;
+    deadline: number;
+    estimatedDelivery: number;
+    confidential: boolean;
+    quantity: number;
+    remuneration: number;
+    serviceFee: number;
+    workSchedule: string;
+    age: number;
+    gender: GenderAbbreviation;
+    jobResponsibilities: string;
+    professionalExperience: string;
+    titleGenerated: boolean;
+    computerSkills: string;
+    languages: string;
+    behavioralProfile: string;
+    workEnvironment: string;
+    additionalNotes: string;
+    restrictions: string;
+    cancellationReason: string;
 }
