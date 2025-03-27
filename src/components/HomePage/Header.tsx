0e952d36b7a47766cf483ea51d1ba46fcc527661
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-03-27 09:03:44 -0300

    fix: header links not working on mobile

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <>
             <header className="sticky top-0 z-50 flex justify-between items-center py-6 px-8 md:px-32 bg-transparent backdrop-blur-md font-roboto">
                 <a className="text-white font-bold hover:scale-105 transition-all">
-                    <img src={logo} className="md:h-12 h-8"/>
+                    <img src={logo} className="md:h-12 h-8" />
                 </a>
 
                 <ul className="hidden xl:flex items-center gap-12 text-base text-gray-200 font-roboto">
@@ -24,12 +24,24 @@ export default function Header() {
                 </svg>
             </header>
             <div className={`fixed text-white xl:hidden top-[72px] left-0 w-full bg-transparent backdrop-blur-md flex flex-col items-center font-roboto transform transition-transform text-lg ${isMenuOpen ? "opacity-100" : "hidden"}`} style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}>
-                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Home</li>
-                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Currículo</li>
-                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Vagas</li>
-                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Serviços</li>
-                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Sobre</li>
-                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">Contato</li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">
+                    <a href="#hero">Home</a>
+                </li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">
+                    <a href="#resume">Currículo</a>
+                </li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">
+                    <a href="#vacancies">Vagas</a>
+                </li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">
+                    <a href="#services">Serviços</a>
+                </li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">
+                    <a href="#about-us">Sobre nós</a>
+                </li>
+                <li className="list-none w-full text-center p-4 transition-all cursor-pointer hover:font-bold">
+                    <a href="#contact">Contato</a>
+                </li>
             </div>
         </>
     )
