d0ebd69eef31e0527d09ee0b6fe9b218254f7594
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-04-02 12:48:34 -0300

    refactor: add links in hero buttons

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -7,11 +7,15 @@ export default function Hero() {
             <div className="w-full py-36 px-8 text-white">
                 <Title variant={"h1"} text={"As melhores vagas da região ao seu alcance."} className={"py-1 w-full md:w-3/4 mx-auto"}></Title>
                 <p className="subtitle md:text-lg text-gray-400 lg:text-xl my-8 sm:w-1/2 mx-auto">
-                Conectamos talentos ao sucesso com compromisso, agilidade e excelência nos serviços personalizados.
+                    Conectamos talentos ao sucesso com compromisso, agilidade e excelência nos serviços personalizados.
                 </p>
                 <div className="cta-buttons mt-8">
-                    <Button text={"Olhar Vagas"}></Button>
-                    <Button variant="dark" text={"Mandar Currículo"}></Button>
+                    <a href="#vacancies">
+                        <Button text={"Olhar Vagas"}></Button>
+                    </a>
+                    <a href="#resume">
+                        <Button variant="dark" text={"Mandar Currículo"}></Button>
+                    </a>
                 </div>
             </div>
         </div>
