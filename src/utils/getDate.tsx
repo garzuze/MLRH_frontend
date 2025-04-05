a388196a7b8c590ba9e38466b5a6cd161745c4d5
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-04-05 09:42:15 -0300

    refactor: improve getDate function

--- src/utils/getDate.tsx
+++ src/utils/getDate.tsx
@@ -1,40 +1,8 @@
 export default function getDate() {
-    const daysWeek: Record<number, string> = {
-        0: "Domingo",
-        1: "Segunda-feira",
-        2: "Terça-feira",
-        3: "Quarta-feira",
-        4: "Quinta-feira",
-        5: "Sexta-feira",
-        6: "Segunda-feira",
-    }
-
-    const months: Record<number, string> = {
-        0: "Janeiro",
-        1: "Fevereiro",
-        2: "Março",
-        3: "Abril",
-        4: "Maio",
-        5: "Junho",
-        6: "Julho",
-        7: "Agosto",
-        8: "Setembro",
-        9: "Outubro",
-        10: "Novembro",
-        11: "Dezembro",
-    }
-
     const today = new Date();
     const dayMonth = today.getDate();
-    const dayWeek = today.getDay();
     const year = today.getFullYear();
     const month = today.getMonth();
-    let monthStr: string;
-    if (month < 10) {
-        monthStr = `0${month + 1}`
-    } else {
-        monthStr = String(month + 1);
-    }
 
-    return `${dayMonth}/${monthStr}/${year}`
+    return `${dayMonth < 10 ? `0${dayMonth + 1}` : String(dayMonth + 1)}/${month < 10 ? `0${month + 1}` : String(month + 1)}/${year}`
 }
