cbe8b9a3f1fe21b9a65195549d104b534e3e7489
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-04-07 11:36:31 -0300

    refactor: implement login inside VerifyEmailPage

--- src/pages/VerifyEmailPage.tsx
+++ src/pages/VerifyEmailPage.tsx
@@ -3,6 +3,8 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import Title from "../components/ui/Title";
 import Button from "../components/ui/Button";
 import { useAxiosClient } from "../hooks/useAxiosClient";
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function VerifyEmailPage() {
     const [verified, setVerified] = useState<boolean>();
@@ -10,7 +12,10 @@ export default function VerifyEmailPage() {
     const uid = searchParams.get("uid");
     const token = searchParams.get("token");
     const hasSentRequest = useRef(false);
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
     const navigate = useNavigate();
+    const { login } = useAuth();
     const axiosClient = useAxiosClient();
 
 
@@ -19,7 +24,8 @@ export default function VerifyEmailPage() {
     useEffect(() => {
         const verifyEmail = async (uid: string, token: string) => {
             try {
-                await axiosClient.get(`api/verify-email?uid=${uid}&token=${token}`);
+                const response = await axiosClient.get(`api/verify-email?uid=${uid}&token=${token}`);
+                setEmail(response.data.email);
                 setVerified(true);
             } catch (error) {
                 console.error(error);
@@ -32,20 +38,48 @@ export default function VerifyEmailPage() {
         }
     }, [uid, token]);
 
-    if (verified) {
-        const handleSubmit = async (e: React.FormEvent) => {
-            e.preventDefault();
-            navigate('/');
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (email && password) {
+            const isLogged = await login(email, password);
+            if (isLogged) {
+                navigate('/curriculo');
+            } else {
+                status = "Houve algum erro"
+            }
         }
+    }
 
+    if (verified) {
         status =
             <div>
-                <Title variant="h2" text="Email validado!"></Title>
-                <form onSubmit={handleSubmit}>
+                <Title variant="h2" text="Email validado!" className="text-center"></Title>
+                <h5 className="subtitle text-center font-normal text-lg text-gray-400 md:text-xl mt-2 mb-8">
+                    Agora você pode cadastar seu currículo.
+                </h5>
                     <div className="mx-auto text-center">
-                        <Button text="Fazer login" type="submit"></Button>
+                        <form onSubmit={handleSubmit} className="bg-neutral-950 border border-neutral-800 rounded-lg shadow md:mt-0 sm:max-w-md p-8">
+                            <label htmlFor="email" className="block mb-2 text-sm text-left font-medium text-zinc-200">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="bg-neutral-950 border border-neutral-800 text-zinc-200 text-sm rounded-lg focus:ring-indigo-950 focus:border-indigo-950 hover:ring-indigo-950 block w-full p-2.5"
+                                required
+                            value={email}
+                            />
+                            <label htmlFor="password" className="block my-2 text-left text-sm font-medium text-zinc-200">Senha</label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="bg-neutral-950 border border-neutral-800 text-zinc-200 text-sm rounded-lg focus:ring-indigo-950 focus:border-indigo-950 ring-indigo-950 block w-full p-2.5"
+                                required
+                                value={password}
+                                placeholder="*******"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button text="Cadastrar currículo" type="submit" className="w-full mx-0"></Button>
+                        </form>
                     </div>
-                </form>
             </div>
     } else {
         status =
@@ -60,7 +94,7 @@ export default function VerifyEmailPage() {
     return (
         <main className="w-full mx-auto min-h-screen grid grid-cols-4">
             <div className="col-span-4">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen font-roboto bg-gradient-to-br from-neutral-950 via-neutral-900 to-indigo-900 lg:py-0">
+                <div className="flex flex-col items-center justify-center px-6 py-8 h-screen font-roboto bg-gradient-to-br from-neutral-950 via-neutral-900 to-indigo-900 lg:py-0">
                     {status}
                 </div>
             </div>
