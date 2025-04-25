985d6550acccc7bd971f6ddf0240cd6721c5d976
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-04-25 17:44:10 -0300

    refactor: improve styling in ResumeSearchInput

--- src/components/ResumePage/ResumeSearchInput.tsx
+++ src/components/ResumePage/ResumeSearchInput.tsx
@@ -1,6 +1,6 @@
-import { ColumnFilter } from "./ColumnFilter";
+import { ColumnFilter } from "../../types/ColumnFilter";
 
-export const ResumeFilters = ({ columnFilters, setColumnFilters }: { columnFilters: ColumnFilter[], setColumnFilters: React.Dispatch<React.SetStateAction<ColumnFilter[]>> }) => {
+export const ResumeSearchInput = ({ columnFilters, setColumnFilters }: { columnFilters: ColumnFilter[], setColumnFilters: React.Dispatch<React.SetStateAction<ColumnFilter[]>> }) => {
     const resumeName = columnFilters?.find((f) => f.id === "name")?.value || "";
 
     const onFilterChange = (id: string, value: string) => setColumnFilters(
@@ -16,6 +16,7 @@ export const ResumeFilters = ({ columnFilters, setColumnFilters }: { columnFilte
                 placeholder="Pesquisar nome..."
                 value={resumeName}
                 onChange={(e) => onFilterChange("name", e.target.value)}
+                className="block py-2 ps-10 text-sm text-stone-900 border border-stone-300 rounded-lg w-80 bg-stone-50 focus:ring-purple-500 focus:border-purple-500"
             ></input>
         </fieldset>
     )
--- src/components/ResumePage/ResumeTable.tsx
+++ src/components/ResumePage/ResumeTable.tsx
@@ -12,8 +12,9 @@ import {
 import { SlimResumeType } from "../../types/SlimResumeType";
 import { useSlimResume } from '../../hooks/useSlimResume';
 import { formatDate } from '../../utils/formatDate';
-import { ResumeFilters } from './ResumeFilters';
-import { ColumnFilter } from './ColumnFilter';
+import { ResumeSearchInput } from './ResumeSearchInput';
+import { ColumnFilter } from '../../types/ColumnFilter';
+import { FiSearch } from 'react-icons/fi';
 export function ResumeTable() {
     const { data: resumes = [], error, isLoading } = useSlimResume();
     const [columnFilters, setColumnFilters] = useState<ColumnFilter[]>([]);
@@ -42,41 +43,53 @@ export function ResumeTable() {
     })
     if (error) return <div>Erro: {error.message}</div>
     return (
-        <div className='space-y-4'>
-            <ResumeFilters columnFilters={columnFilters} setColumnFilters={setColumnFilters} />
-            <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
-                <table className='w-full text-sm text-left text-stone-500 rounded'>
-                    <thead className='text-xs text-stone-700 uppercase bg-stone-50 rounded'>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <tr key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => (
-                                    <th key={header.id} scope='col' className='px-6 py-3 text-center'>
-                                        {flexRender(header.column.columnDef.header, header.getContext())}
-                                    </th>
-                                ))}
-                            </tr>
-                        ))}
-                    </thead>
-                    <tbody>
-                        {table.getRowModel().rows.map((row) => (
-                            <tr key={row.id} className='bg-white border-b border-stone-200 hover:bg-stone-50'>
-                                {row.getVisibleCells().length > 0 ? (
-                                    row.getVisibleCells().map((cell) => {
-                                        return (
-                                            <td {...{
-                                                key: cell.id,
-                                                className: cell.id.endsWith('name') ? 'px-2 py-2 font-medium text-stone-900 whitespace-nowrap' : 'px-2 py-2',
+        <div className='space-y-4 px-4 bg-white'>
+            {/* <ResumeFilters columnFilters={columnFilters} setColumnFilters={setColumnFilters} /> */}
+            <div className=''>
+                <div className="pb-4 bg-white">
+                    <label htmlFor="table-search" className="sr-only">Search</label>
+                    <div className="relative mt-1">
+                        <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <FiSearch />
+                        </div>
+                        <ResumeSearchInput columnFilters={columnFilters} setColumnFilters={setColumnFilters} />
+                    </div>
+                </div>
+                <div className='rounded shadow-md relative x-auto'>
+                    <table className='w-full text-sm text-left text-stone-500'>
+                        <thead className='text-xs text-stone-700 uppercase bg-stone-50 rounded'>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <tr key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => (
+                                        <th key={header.id} scope='col' className='px-6 py-3 text-center'>
+                                            {flexRender(header.column.columnDef.header, header.getContext())}
+                                        </th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody>
+                            {table.getRowModel().rows.map((row) => (
+                                <tr key={row.id} className='bg-white border-b border-stone-200 hover:bg-stone-50'>
+                                    {row.getVisibleCells().length > 0 ? (
+                                        row.getVisibleCells().map((cell) => {
+                                            return (
+                                                <td {...{
+                                                    key: cell.id,
+                                                    className: cell.id.endsWith('name') ? 'px-2 py-2 font-medium text-stone-900 whitespace-nowrap' : 'px-2 py-2',
 
-                                            }}>
-                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                            </td>
-                                        )
-                                    })) : "Não há registros"
-                                }
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                                                }}>
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </td>
+                                            )
+                                        })) : "Não há registros"
+                                    }
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                </div>
             </div>
             <div>
                 <p>
