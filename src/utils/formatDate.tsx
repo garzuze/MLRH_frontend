18b6a55d512fb88aa0ca7f87574305ebcf1e06de
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-05-20 22:30:58 -0300

    fix: update formatDate to handle empty parameters

--- src/utils/formatDate.tsx
+++ src/utils/formatDate.tsx
@@ -1,4 +1,10 @@
-export const formatDate = (dateString: string) => {
+export const formatDate = (dateString: string | null | undefined) => {
+    if (!dateString) {
+        return "-";
+    }
     const [year, month, day] = dateString.split("-");
+    if (!year || !month || !day) {
+        return "-";
+    }
     return `${day}/${month}/${year}`;
 };
