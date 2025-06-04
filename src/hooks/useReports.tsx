111bd7ef274fd35f6f6324e932b08feaeee854a7
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2025-06-04 20:21:51 -0300

    refactor: differenciate queryKeys in useReports

--- src/hooks/useReports.tsx
+++ src/hooks/useReports.tsx
@@ -11,12 +11,12 @@ interface Filter {
 export const useReports = (filter?: Filter, options = { enabled: true }) => {
     const { token } = useAuth();
     const axiosClient = useAxiosClient();
-    const queryKey = filter?.id ? ['reports', token, filter?.id] : ['reports', token];
-
+    const queryKey = filter?.invoiceable ? ['invoiceable_reports'] : ['reports']
+    token && queryKey.push(token);
     return useQuery<ReportType[]>({
         queryKey: queryKey,
         queryFn: async () => {
-            const url = filter?.id ? `hr/report/?id=${Array.isArray(filter?.id) ? filter?.id.join(",") : filter?.id}` : filter?.invoiceable ? "hr/report/?invoiceable=true" :"hr/report/"
+            const url = filter?.id ? `hr/report/?id=${Array.isArray(filter?.id) ? filter?.id.join(",") : filter?.id}` : filter?.invoiceable ? "hr/report/?invoiceable=true" : "hr/report/"
             const response = await axiosClient.get(url);
             return response.data;
         },
